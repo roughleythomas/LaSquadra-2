fc0f6b04c9cfb3fe874b0902281aaeea20c0be64
Spice <mvanlaethem0@gmail.com>
2022-02-16 12:21:46 -0800

    Created files for joystick

---
+++ c8051SwiftGL/Joystick.h
@@ -0,0 +1,12 @@
+//
+//  Joystick.h
+//  c8051SwiftGL
+//
+//  Created by Martin Van Laethem on 2022-02-16.
+//
+
+#ifndef Joystick_h
+#define Joystick_h
+
+
+#endif /* Joystick_h */
---
+++ c8051SwiftGL/Joystick.mm
@@ -0,0 +1,8 @@
+//
+//  Joystick.m
+//  c8051SwiftGL
+//
+//  Created by Martin Van Laethem on 2022-02-16.
+//
+
+#import <Foundation/Foundation.h>
